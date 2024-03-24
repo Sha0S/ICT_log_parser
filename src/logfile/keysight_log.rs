cba1967671cf1ac026916bd736752f1147a582cb
Bernát Süli <suli.bernat@gmail.com>
2024-03-24 21:35:20 +0100

    Analog measurements

--- src/logfile/keysight_log.rs
+++ src/logfile/keysight_log.rs
@@ -5,9 +5,51 @@ Implement special characters '~' (literal field) and '\' (list of fields)
 
 use std::{fs, io, path::Path, str::Chars};
 
-enum KeysightPrefix {
-    //{@A-CAP|test status|measured value|subtest designator}
-    ACap(i32, f32, Option<String>),
+pub enum AnalogTest {
+    Cap,         // A-CAP
+    Diode,       // A-DIO
+    Fuse,        // A-FUS
+    Inductor,    // A-IND
+    Jumper,      // A-JUM
+    Measurement, // A-MEA
+    NFet,        // A-NFE
+    PFet,        // A-PFE
+    Npn,         // A-NPN
+    Pnp,         // A-PNP
+    Pot,         // A-POT
+    Res,         // A-RES
+    Switch,      // A-SWI
+    Zener,       // A-ZEN
+
+    Error,
+}
+
+impl From<&str> for AnalogTest {
+    fn from(value: &str) -> Self {
+        match value {
+            "@A-CAP" => Self::Cap,
+            "@A-DIO" => Self::Diode,
+            "@A-FUS" => Self::Fuse,
+            "@A-IND" => Self::Inductor,
+            "@A-JUM" => Self::Jumper,
+            "@A-MEA" => Self::Measurement,
+            "@A-NFE" => Self::NFet,
+            "@A-PFE" => Self::PFet,
+            "@A-NPN" => Self::Npn,
+            "@A-PNP" => Self::Pnp,
+            "@A-POT" => Self::Pot,
+            "@A-RES" => Self::Res,
+            "@A-SWI" => Self::Switch,
+            "@A-ZEN" => Self::Zener,
+
+            _ => Self::Error,
+        }
+    }
+}
+
+pub enum KeysightPrefix {
+    // {@A-???|test status|measured value|subtest designator}
+    Analog(AnalogTest, i32, f32, Option<String>),
 
     UserDefined(Vec<String>),
     Error(Vec<String>),
@@ -33,31 +75,34 @@ fn to_float(field: Option<&String>) -> Option<f32> {
     None
 }
 
+fn get_string(data: &[String], index: usize) -> Option<String> {
+    if data.len() >= index {
+        Some(data[index].clone())
+    } else {
+        None
+    }
+}
+
 impl KeysightPrefix {
     fn new(data: Vec<String>) -> Option<Self> {
         if let Some(first) = data.first() {
             match first.as_str() {
-                //{@A-CAP|test status|measured value|subtest designator}
-                "@A-CAP" => {
+                // {@A-???|test status|measured value|subtest designator} designator is optional
+                "@A-CAP" | "@A-DIO" | "@A-FUS" | "@A-IND" | "@A-JUM" | "@A-MEA" | "@A-NFE"
+                | "@A-PFE" | "@A-NPN" | "@A-PNP" | "@A-POT" | "@A-RES" | "@A-SWI" | "@A-ZEN" => {
                     let status = to_int(data.get(1));
                     let result = to_float(data.get(2));
-                    if status.is_some() && result.is_some() {
-                        let designator = {
-                            if data.len() > 3 {
-                                Some(data[3].clone())
-                            } else {
-                                None
-                            }
-                        };
-
-                        Some(KeysightPrefix::ACap(
-                            status.unwrap(),
-                            result.unwrap(),
-                            designator,
-                        ))
-                    } else {
-                        Some(KeysightPrefix::Error(data.clone()))
+                    if let Some(s) = status {
+                        if let Some(r) = result {
+                            return Some(KeysightPrefix::Analog(
+                                data[0].as_str().into(),
+                                s,
+                                r,
+                                get_string(&data, 3),
+                            ));
+                        }
                     }
+                    Some(KeysightPrefix::Error(data.clone()))
                 }
 
                 _ => Some(KeysightPrefix::UserDefined(data.clone())),
